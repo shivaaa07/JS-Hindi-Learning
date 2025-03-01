ed7af917e125b75b6afd41443286c062825c812b
Shivkumar Sahani <153889887+shivaaa07@users.noreply.github.com>
2025-03-01 02:50:49 +0000

    variable Learned

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553;
+let accountEmail = "shivaa@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 9076123;     // Not Allowed
+
+accountEmail = "jyotidas@gmail.com";
+accountPassword = "JyotiDas@27";
+accountCity = "Mumbai";
+
+{
+    let accountEmail = "shiva@google.com";
+    // console.log(accountEmail);
+}
+
+// console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
