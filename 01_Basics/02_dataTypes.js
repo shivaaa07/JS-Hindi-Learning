1e4d3db538b14667d372aa5419caf8bca77445fd
Shivkumar Sahani <153889887+shivaaa07@users.noreply.github.com>
2025-03-02 02:43:22 +0000

    Data Type Learned

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"        // treat all JS code as newer version
+
+// alert("Shiva Sahani");      // we are using node.js not broswer
+
+// console.log( 3 + 3 )  console.log("Shivaa")        // This is not a good practies to write code in single line
+
+console.log(
+    3
+    +
+    3
+);              // Code readability should be high and JS remove the line break and white space but this is not good practies
+
+
+let userName = "Shiva Sahani";
+let age = 28;
+let isLoggedIn = false;
+
+/*
+1) String
+2) Number => 2 to Power 53
+3) bigInt
+4) Boolean
+5) null => standalone value
+6) undefined => undefined
+7) symbol
+
+++++++++++++++ Non-Primitive Data Type ++++++++++++++++++++
+1) Object
+2) Array
+*/
+
+console.log(typeof undefined);
+
+
+
