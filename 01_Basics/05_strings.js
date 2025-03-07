f5147b13298059f59702de35256d26ba5ffd026f
Shivkumar Sahani <153889887+shivaaa07@users.noreply.github.com>
2025-03-07 02:30:41 +0000

    String Interpolation

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,35 @@
+/*
++++++++++++++++++++++ String Prototype Learn ++++++++++++++++++++++++++++
+1) charAt()
+2) concat()
+3) at()
+4) constructor
+5) includes()
+6) indexOf()
+7) lastIndexOf()
+8) match()
+9) matchAll()
+10) repeat()
+11) replace()
+12) replaceAll()
+13) search()
+14) slice()
+15) split()
+16) sub()
+17) substr()
+18) substring()
+19) toString()
+*/
+
+
+const userName = "Shiva";
+const repoCount = 50;
+
+// console.log("My name is " + userName.toUpperCase() + " " + "and my git repoCount is " + repoCount + " value");
+// String interpolation or template litteral
+
+// console.log(`My name is ${userName.toUpperCase()} and my git repoCount is ${repoCount}`);
+
+const gameName = new String("shivass");
+
+console.log(gameName);
