8f7215165f62ff5e864e7cdc7755d0c6d0d444c4
Shivkumar Sahani <153889887+shivaaa07@users.noreply.github.com>
2025-03-10 03:23:46 +0000

    Split method

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -5,7 +5,7 @@ let repoCount = 50;
 
 // String Interpolation
 
-console.log(`My name is ${name.toUpperCase()} and my repo count is ${repoCount}.`);
+// console.log(`My name is ${name.toUpperCase()} and my repo count is ${repoCount}.`);
 
 const gameName = new String("hitesh-hc-com");
 
@@ -21,8 +21,27 @@ const gameName = new String("hitesh-hc-com");
 
 // console.log(gameName.includes("Hitesh"))
 
-// console.log(gameName.substring(-2, 6));
-// console.log(gameName.slice(-2, 6));
+// console.log(gameName.substring(5, 3));
 
+console.log(gameName);
 
-console.log(gameName.split("-"))
+// console.log(gameName.slice(5, 3));
+
+console.log(gameName.replaceAll("-", "_"))
+
+// console.log(gameName.split("-"))
+
+/*
+    String Methods in JavaScript
+    1) charAt()
+    2) at()
+    3) Repeat()
+    4) match()
+    5) indexOf()
+    6) concat()
+    7) subString()
+    8) slice()
+    9) replace()    - Only First Match will be replace
+    10) replaceAll() - Replcae All Match will be replace
+    11) 
+*/
