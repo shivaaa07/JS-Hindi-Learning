ff72b90bf9634cb9a93c52c8d1c4f512a97c5a84
Shivkumar Sahani <153889887+shivaaa07@users.noreply.github.com>
2025-03-11 01:47:57 +0000

    let, const and var ki kahani

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,21 +1,20 @@
-const accountId = 123;
-let accountEmail = "shiva@google.com";
-var accountPassword = "Shiva#123";
+const accountId = 12345;
+let accountEmail = "shivasahani@gmail.com";
+var accountPassword = "Sahani#123";
 accountCity = "Jaipur";
 let accountState;
 
-// accountId = 097;        // Not Allowed to re-assign the value of constant variable
+// accountId = 2;
 
-accountEmail = "ss@microsoft.com";
-accountPassword = "Sahani@123";
-accountCity = "Mumbai";
+// console.log(accountId);  // Not Allowed to re-assign to constant variable
 
-// console.log(accountId);
-
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
-
+accountEmail = "shiva@google.com";
+accountPassword = "Google#321";
+accountCity = "Bengaluru";
 
 /*
-    Prefer not to use "var" keyword for assign the variable in JS 
-    because of issue in block scope and functional scope 
+    prefer not to use var keyword 
+    because of issue in block scope and functional scope
 */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
