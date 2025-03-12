283ebb78040a183caaa95e3a83d848a6d488ca16
shivaaa07 <shivaawork07@gmail.com>
2025-03-12 20:41:55 +0530

    Math and Number are complated

--- 01_Basics/06_numbers_and_math.js
+++ 01_Basics/06_numbers_and_math.js
@@ -1,16 +1,17 @@
 const score = 40000;
 
-// console.log(score.toExponential(2));
+console.log(score.toExponential(2));
 
-// console.log(score.toFixed(2));
-// console.log(score.toLocaleString("en-IN"));
-// console.log(score.toString().length);
+console.log(score.toFixed(2));
+console.log(score.toLocaleString("en-IN"));
+console.log(score.toString().length);
 
 // ++++++++++++++++++++++++ Maths ++++++++++++++++++++++++
 
 const minNumber = 1;
 const maxNumber = 6;
-const randomNumber = Math.round(Math.random() * maxNumber);
+const randomNumber = Math.floor(Math.random() * maxNumber);
 const dicsNumber = randomNumber + minNumber;
-console.log(randomNumber);
-console.log(dicsNumber);
+
+// console.log(randomNumber);
+console.log("The Dics Number is :", dicsNumber);
