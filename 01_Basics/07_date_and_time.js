d95066dfe9ac7e2eab88efbb50f2766f3130ca9d
Shivkumar Sahani <153889887+shivaaa07@users.noreply.github.com>
2025-03-13 03:34:19 +0000

    Date and Time

---
+++ 01_Basics/07_date_and_time.js
@@ -0,0 +1,9 @@
+// Coordinated Universal Time (UTC) - Starting Date January 1, 1970
+
+const myDate = new Date();
+
+console.log(myDate);
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleString())
