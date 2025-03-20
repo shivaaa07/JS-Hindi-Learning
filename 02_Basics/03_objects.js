651da73e83cb160fe8e05aa596e452c3574ce6e4
shivaaa07 <shivaawork07@gmail.com>
2025-03-20 08:11:48 +0530

    Object Basic learn

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,35 @@
+// Singleton
+// constructor()
+
+// Object Literals
+
+const mySym = Symbol("123");
+
+const jsUser = {
+  name: "Shiva",
+  "full name": "Shiva Sahani",
+  age: 28,
+  location: "Jaipur",
+  email: "shivaa@google.com",
+  isLoggedIn: false,
+  [mySym]: "Hello World!",
+};
+
+// console.log(jsUser);
+// console.log(jsUser["location"]);
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
+
+// Object.freeze(jsUser); // freeze method cannot change the object value after the freezing and freeze is static method of object
+
+jsUser["full name"] = "Shivkumar Sahani";
+
+jsUser.greeting = function () {
+  return `Hello JS user`;
+};
+jsUser.greetingTwo = function () {
+  return `Hello JS user, ${this["full name"]}`;
+};
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
