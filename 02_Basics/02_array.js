b02a845b722789b2c432bf334ec20428e30936fb
shivaaa07 <shivaawork07@gmail.com>
2025-03-24 11:58:47 +0530

    Array Static Method Revision

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -1,33 +1,41 @@
-const sportBike = ["R15", "MT15", "Z900", "ZX10R"];
+const classic_Bike = ["bajaj platina", "Splender", "Yamaha RX100"];
 
-const classicBike = ["Royal Enfield 350", "Yamaha RX100", "Splender", "Bajaj Platina"];
+const sport_bike = ["Yamaha MT15", "Yamaha R15", "Royal Enfield 350"];
 
-// sportBike.push(classicBike);
+// classic_Bike.push(sport_bike);
 
-const allBikes = sportBike.concat(classicBike); 
+// console.log(classic_Bike);
+// console.log(classic_Bike[3][1]);
 
-const allBikeCollections = [...sportBike, ...classicBike];    // Speard Operator
+// const allBikes = classic_Bike.concat(sport_bike);
+const allBikes = [...classic_Bike, ...sport_bike]; // Speard operator
 
-// console.log(sportBike);
 // console.log(allBikes);
-// console.log(allBikeCollections);
 
-// nested Array
-const nestedArray = [1, 2, 3, [4, 5, 6, [7, 9, 8], 10, 11], 12, [13, 14, [115, 16, 117], 18, 19], 20];
+const nestedArray = [
+  1,
+  2,
+  3,
+  4,
+  [5, 6, 7, [8, 9, 10], 11, 12],
+  14,
+  13,
+  [15, 16, [17, 18, [19, 20, 21], 22, 23], 21],
+];
 
-const singleArray = nestedArray.flat(2);
+const allArrayInSingle = nestedArray.flat(3);
 
-// console.log(nestedArray);
-// console.log(singleArray);
+// console.log(allArrayInSingle);
 
-// +++++++++++++++++++++ Static Methods ++++++++++++++++++++++
+// ++++++++++++++++++++++++ Array Static Methhods ++++++++++++++++++++
 
-// console.log(Array.isArray("Shiva"));    // check given value is array or not
+// console.log(Array.isArray("Shiva"));        // check the value is Array or not
 
-const createdArrayFromMethod = Array.from([1, 2, 3, 4], nums => nums*0.5);    
+// console.log(Array.from({ name: "Shiva" }));
 
-// console.log(createdArrayFromMethod);
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+let score4 = 400;
 
-const createdArrayWithOfMethod = Array.of("Shiva", "Sahani", "Azim");
-
-console.log(createdArrayWithOfMethod)
+console.log(Array.of(score1, score2, score3, score4));
