1ea225fffdb15b5736765b59cf73e3e596c4fef4
Shivkumar Sahani <153889887+shivaaa07@users.noreply.github.com>
2025-03-25 02:25:15 +0000

    Objects

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -1,44 +1,39 @@
 // Singleton
-// constructor()
+// const singletonObject = Object.create();
+
+const myObj =  new Object()
+// console.log(singletonObject);
+// console.log(myObj);
 
 // Object Literals
 
-const mySym = Symbol("123");
+const mySym = Symbol(123);
 
 const jsUser = {
   name: "Shiva",
-  "full name": "Shiva Sahani",
   age: 28,
-  location: "Jaipur",
-  email: "shivaa@google.com",
+  email: "shiva@gmail.com",
+  "full Name": "Shivaa Sahani",
   isLoggedIn: false,
-  [mySym]: "Hello World!",
+  [mySym]: "Jyoti"
 };
 
 // console.log(jsUser);
-// console.log(jsUser["location"]);
-// console.log(jsUser["full name"]);
 // console.log(jsUser[mySym]);
 
-// Object.freeze(jsUser); // freeze method cannot change the object value after the freezing and freeze is static method of object
+// Object.freeze(jsUser)   // after the freeze method Object are Immutable
+jsUser.email = "shiva.sahani@googgle.com";
 
-jsUser["full name"] = "Shivkumar Sahani";
+jsUser.greetng = function() {
+  console.log("Hello JS User!");
+}
 
-jsUser.greeting = function () {
-  return `Hello JS user`;
-};
-jsUser.greetingTwo = function () {
-  return `Hello JS user, ${this["full name"]}`;
-};
+jsUser.greetngTwo = function() {
+  console.log(`Hello JS User!, ${this["full Name"]}`);
+}
 
-// console.log(jsUser.greeting());
-// console.log(jsUser.greetingTwo());
+// console.log(jsUser);
 
-// const myObj = new Object();
-// const myObj = new Object(null);
-// const myObj = new Object(undefined);
-const myObj = new Object(BigInt);
+// console.log(jsUser.greetng)
 
-// myObj["name"] = "Shiva";
-
-console.log(myObj);
+jsUser.greetngTwo()
