f118350348678fab749f9891a8d56ce2800d0e05
shivaaa07 <shivaawork07@gmail.com>
2025-03-27 09:19:58 +0530

    Function Final Parts Completed

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -37,7 +37,31 @@ function addTwoNumber(number1 = 12, number2 = 12) {
   return `The Addition of ${number1} and ${number2} is ${number1 + number2}.`;
 }
 
-console.log(addTwoNumber(1, 23));
+// console.log(addTwoNumber(1, 23));
+
+// function with rest operator in parameter
+function calculateCardPrice(...num) {
+  let result = 0;
+  num.forEach((element) => {
+    // console.log(element);
+    result += element;
+  });
+  return result.toFixed(2);
+}
+
+// console.log(calculateCardPrice(200, 400, 100, 1000));
+
+const users = {
+  name: "Shiva",
+  age: 28,
+};
+
+function handleObject(anyObject) {
+  return `My name is ${anyObject.name} and my age is ${anyObject.age}`;
+}
+
+// console.log(handleObject(users));
+console.log(handleObject({ name: "Jyoti Das", age: 26 }));
 
 /*
 {} => Curly Braces
