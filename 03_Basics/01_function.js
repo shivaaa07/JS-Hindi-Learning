7e42809d5fc49374555dad57681bf793096f11e1
shivaaa07 <shivaawork07@gmail.com>
2025-03-27 08:59:59 +0530

    function first part completed

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -1,40 +1,46 @@
 /*
-  1) What is Function in JavaScript
-  Ans) In JS, a function is a reusable block of code design to perform specific task.
-  => A function definition is also called a function declaration or function statement consist of the funtion keyword.
+  1) Whhat is Function
+  ans) In JavaScript, a function is a reusable block of code designed to perform specific task.
 */
 
-// Normal Funtion
+function printMyName() {
+  console.log("S");
+  console.log("H");
+  console.log("I");
+  console.log("V");
+  console.log("A");
+  console.log("A");
+  console.log("\n");
+}
+
+// printMyName();
+
 function greeting() {
-  return `Welcome to JS Youtube Serise by Hitesh Sir.`
+  console.log("Hello JS User💕");
 }
-// console.log(greeting());
+// greeting();
 
-// Funtion with Parameter
-function squareOfNumber(number) {
-  return `The Square of number ${number} is : ${number * number}`;
+// function with parameter
+function square(num) {
+  // Parameter
+  return `The Square of number ${num} is : ${num * num}.`;
 }
 
-// console.log(squareOfNumber(4));
+// console.log(square(12));      // Argument
+
+// function addTwoNumber(number1, number2) {
+//   return `The Addition of ${number1} and ${number2} is ${number1 + number2}.`;
+// }
 
 // function with opational parameter
-function addTwoNumber(num1 = 10, num2 = 21) {
-  return `The Sum of Two number is : ${num1 + num2}`
-}
-// console.log(addTwoNumber(12, 12));
-
-// let name = "Jyoti Das";
-
-const funObj = {
-  name: "Shiva",
-  age: 18,
-  arrowFunction: ()=> {
-    console.log(`Hello ${this.name}`)
-  },
-  normalFuntion: function() {
-    console.log(`Welcome ${this.name}`);
-  }
+function addTwoNumber(number1 = 12, number2 = 12) {
+  return `The Addition of ${number1} and ${number2} is ${number1 + number2}.`;
 }
 
-funObj.arrowFunction();
-funObj.normalFuntion();
+console.log(addTwoNumber(1, 23));
+
+/*
+{} => Curly Braces
+[] => Square Bracket
+() => Pare  ntheses 
+*/
