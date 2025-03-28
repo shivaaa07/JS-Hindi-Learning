ada4d1341b7bcdd735ffd779216a0f2ad2d0e5c3
Shivkumar Sahani <153889887+shivaaa07@users.noreply.github.com>
2025-03-28 02:38:40 +0000

    scope in ifelse statment

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -1,3 +1,24 @@
-let a = 10;
-const b = 20;
-var c = 30;
+// import { addToNumber } from "app.js"
+
+// var c = 300;     // var keyword are not to use because of it's a globle scope.
+
+if (1) {
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+if (true) {
+    let userName = "Shiva";
+    if (userName === "Shiva") {
+        let lastName = " Sahani";
+        // console.log(userName + lastName);
+    }
+    // console.log(lastName);
+}
+
+// console.log(userName);
